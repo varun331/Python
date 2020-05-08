ac6db775abf4d68ada3204d896ac12fad6b0c5f7
varun331 <varun.nagpal@rga.com>
2020-05-08 14:39:07 -0400

    '582020'

--- missingtimesheet.R
+++ missingtimesheet.R
@@ -19,7 +19,7 @@ res.mca <- MCA(Rawdata,quali.sup=1:12,ncp=Inf)
 
 plot(res.mca,invisible=c("ind"),cex=.8,autoLab = "y",selectMod = "cos2 20")
 plot(res.mca,invisible=c("quanti.sup","quali.sup"),cex=.8,autoLab = "y",selectMod = "cos2 20")
-plot(res.mca,label = c("var","ind"),cex=.8,autoLab = "y",selectMod = "contrib 30")
+plot(res.mca,label = c("var","ind"),cex=.8,selectMod = "contrib 10")
 
 summary(res.mca,nbelements = Inf,ncp=50,file="MCAresults.txt")
 summary(res.mca,nbelements = Inf,file="MCAresults.txt")
@@ -43,4 +43,4 @@ plot(res.hcpc,axes=c(1 ,2))
 res.hcpc$desc.ind
 write.infile(dimdesc(res.mca,axes=1:10),file="C:\\Users\\varunn\\Documents\\Confluence attachments\\Finance service research\\dimresults.csv",sep = ';',append=FALSE,nb.dec=4)
 write.infile(res.mca,file="C:\\Users\\varunn\\Documents\\Confluence attachments\\Finance service research\\allbankmcaresults.csv",sep = ';',append=FALSE,nb.dec=4)
-write.infile(res.hcpc$data.clust,file="C:\\Users\\varunn\\Documents\\Confluence attachments\\Finance service research\\clusterdata.csv",sep = ';',append=FALSE,nb.dec=4)
+write.infile(res.hcpc$data.clust,file="C:\\Users\\varunn\\Documents\\Confluence attachments\\Finance service research\\clusterdata1.csv",sep = ';',append=FALSE,nb.dec=4)
